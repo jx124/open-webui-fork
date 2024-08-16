fb0856db4d7d2da9de35291add8a69ef03973029
Jie Xiang <n.jiexiang@gmail.com>
2024-08-16 23:39:44 +0800

    Revert "Add AWS Amplify adapter"
    This reverts commit fe0cbb38e4836f8fe9cc3c15c235cd6f6570ea08.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from 'amplify-adapter';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
